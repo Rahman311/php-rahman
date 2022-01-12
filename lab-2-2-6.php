cb72a5b3db423e861b77c33ba4fd3abec9e7c64d
Rahman311 <79744707+Rahman311@users.noreply.github.com>
2022-01-12 21:33:59 +0600

    ЛР2

---
+++ lab-2-2-6.php
@@ -0,0 +1,8 @@
+<p> Рахман Л.А. ПИ-322 <p>
+<?php
+include ('2-Library.php');
+Zad ();
+Mas();
+Viv();
+Obr();
+?>
---
+++ lab-2-5.php
@@ -0,0 +1,12 @@
+<p> Рахман Л.А. ПИ-322 </p>
+<?php
+$var = 5; $i = 0;
+while (++$i <= $var)
+ { echo $i . ' '; }
+?>
+<br>
+<?php
+$var = 1; $i = 6;
+while (--$i >= $var)
+ { echo $i . ' '; }
+?>
